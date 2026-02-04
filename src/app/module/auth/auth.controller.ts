09345d0a3f89f6ba579d3ed6d87d167f29f74d49
hamimBhai742 <mdhamim5088@gmail.com>
2026-02-04 16:54:13 +0600

    Update

--- src/app/module/auth/auth.controller.ts
+++ src/app/module/auth/auth.controller.ts
@@ -2,13 +2,13 @@ import { Request, Response } from "express";
 import { createAsyncFn } from "../../utils/create.async.fn";
 import { authServices } from "./auth.services";
 import { sendResponse } from "../../utils/sendResponse";
-
+import httpStatus from 'http-status'
 const loginUserFromDB=createAsyncFn(async(req:Request,res:Response)=>{
 const user=await authServices.loginUserFromDB(req.body)
 
 sendResponse(res,{
     success:true,
-    statusCode:200,
+    statusCode:httpStatus.OK,
     message:"Login user successfully",
     data:user
 })
--- src/app/module/auth/auth.routes.ts
+++ src/app/module/auth/auth.routes.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { authController } from "./auth.controller";
+import { validateRequest } from "../../middleware/validation";
+import { userLoginZodSchema } from "./auth.zod.validation";
 
 const router=Router()
 
-router.post("/login",authController.loginUserFromDB)
+router.post("/login", validateRequest(userLoginZodSchema),authController.loginUserFromDB)
 
 export const authRoutes=router
